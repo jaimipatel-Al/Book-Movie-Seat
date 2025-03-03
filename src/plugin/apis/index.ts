6636d84b5e6279873f6676db377d05accba62066
Jaimi Patel <jaimip.albiorix@gmail.com>
2025-03-03 19:13:00 +0530

    Create New Password for Theater

--- src/plugin/apis/index.ts
+++ src/plugin/apis/index.ts
@@ -2,8 +2,11 @@ const requests = {
   register: 'auth/register',
   login: 'auth/login',
 
+  verifyToken :'auth/verify-reset-token/',
+  setPassword :'auth/set-password',
+
   addTheater: 'auth/register-sub-admin',
-  editTheater: 'auth/register-sub-admin',
+  editTheater: 'auth/register-sub-admin', //sdfdsdfsdfjkferjgfjhfsjhfgsdjhfgsdjhfg
   getTheater: 'auth/profile',
 
   updateUserProfile:'auth/update-profile'
--- src/views/auth/CreatePassword.vue
+++ src/views/auth/CreatePassword.vue
@@ -1,34 +1,77 @@
 <script setup lang="ts">
-import { EyeIcon, EyeSlashIcon, ArrowPathIcon } from "@heroicons/vue/24/solid";
-import { Form, Field } from "vee-validate";
-import * as yup from "yup";
-import { ref } from "vue";
-import { useRouter } from 'vue-router';
+  import { EyeIcon, EyeSlashIcon, ArrowPathIcon } from "@heroicons/vue/24/solid";
+  import { Form, Field } from "vee-validate";
+  import * as yup from "yup";
+  import { onMounted, ref } from "vue";
+  import { useRoute, useRouter } from "vue-router";
+  import Axios from "@/plugin/axios";
+  import api from "@/plugin/apis";
+  import toast from "@/plugin/toast";
 
-const router=useRouter()
+  const route = useRoute();
+  const router = useRouter();
 
-const schema = yup.object({
-  Password: yup
-    .string()
-    .required()
-    .min(8)
-    .matches(
-      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{6,}$/,
-      "Password must contain at least one uppercase letter, one lowercase letter, one number, and one special character"
-    ),
-  ConfirmPassword: yup
-    .string()
-    .required("Confirm Password is required")
-    .oneOf([yup.ref("Password"), ""], "Passwords must match"),
-});
+  const schema = yup.object({
+    Password: yup
+      .string()
+      .required()
+      .min(8)
+      .matches(
+        /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{6,}$/,
+        "Password must contain at least one uppercase letter, one lowercase letter, one number, and one special character"
+      ),
+    ConfirmPassword: yup
+      .string()
+      .required("Confirm Password is required")
+      .oneOf([yup.ref("Password"), ""], "Passwords must match"),
+  });
 
-const isLoading=ref(false)
-const password = ref("");
-const confirmPassword = ref("");
-const isPassword = ref(false);
-const isConfirmPassword = ref(false);
+  const isVerifying = ref(false);
+  const isLoading = ref(false);
+  const password = ref("");
+  const confirmPassword = ref("");
+  const isPassword = ref(false);
+  const isConfirmPassword = ref(false);
 
-const createPassword = () => {};
+  const createPassword = async () => {
+    isLoading.value = true;
+
+    const user = {
+      token: route.query.token,
+      newPassword: password.value,
+    };
+
+    await Axios.post(api.setPassword, user)
+      .then(({ data }) => {
+        toast.success(data?.message ?? "Your Password Created Successfully!");
+        router.push('/user/profile')
+      })
+      .catch((er) => {
+        toast.error(er?.response?.data?.message ?? "Password Can't Create!");
+      })
+      .finally(() => {
+        isLoading.value = false;
+      });
+  };
+  const verifyToken = async () => {
+    isVerifying.value = true;
+
+    await Axios.get(`${api.verifyToken}${route.query.token}`)
+      .then(({ data }) => {
+        if(data.success)
+        toast.success('Please Create New Password.')
+      })
+      .catch((er) => {
+        // Not Verifying
+      })
+      .finally(() => {
+        isVerifying.value = false;
+      });
+  };
+
+  onMounted(() => {
+    if (route?.query?.token) verifyToken();
+  });
 </script>
 
 <template>
@@ -39,14 +82,8 @@ const createPassword = () => {};
 
       <label for="password">Password</label>
       <div class="input flex items-center justify-between">
-        <Field
-          v-model="password"
-          :type="isPassword ? 'text' : 'password'"
-          name="Password"
-          id="password"
-          class="w-full outline-none"
-          placeholder="Enter Your Password"
-        />
+        <Field v-model="password" :type="isPassword ? 'text' : 'password'" name="Password" id="password"
+          class="w-full outline-none" placeholder="Enter Your Password" />
         <div class="pl-2" @click="isPassword = !isPassword">
           <EyeSlashIcon v-if="isPassword" class="password-icon" />
           <EyeIcon v-else class="password-icon" />
@@ -55,14 +92,8 @@ const createPassword = () => {};
       <p class="error-message">{{ errors?.Password }}</p>
       <label for="confirmPassword">Confirm Password</label>
       <div class="input flex items-center justify-between">
-        <Field
-          v-model="confirmPassword"
-          :type="isConfirmPassword ? 'text' : 'password'"
-          name="ConfirmPassword"
-          id="confirmPassword"
-          class="w-full outline-none"
-          placeholder="Enter Your Confirm Password"
-        />
+        <Field v-model="confirmPassword" :type="isConfirmPassword ? 'text' : 'password'" name="ConfirmPassword"
+          id="confirmPassword" class="w-full outline-none" placeholder="Enter Your Confirm Password" />
         <div class="pl-2" @click="isConfirmPassword = !isConfirmPassword">
           <EyeSlashIcon v-if="isConfirmPassword" class="password-icon" />
           <EyeIcon v-else class="password-icon" />
@@ -70,7 +101,9 @@ const createPassword = () => {};
       </div>
       <p class="error-message">{{ errors?.ConfirmPassword }}</p>
 
-      <button type="submit" :disabled="isLoading"><ArrowPathIcon v-if="isLoading" class="r-w-8 mr-2" /> Create Password</button>
+      <button type="submit" :disabled="isLoading">
+        <ArrowPathIcon v-if="isLoading" class="r-w-8 mr-2" /> Create Password
+      </button>
     </Form>
   </div>
 </template>
